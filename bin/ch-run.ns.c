3f46456d7a1653e120138c5a2dfac9dbdb7e7e79
Reid Priedhorsky <reidpr@lanl.gov>
2016-02-08 15:45:28 -0700

    pivot_root(2) with CLONE_USER works

--- bin/ch-run.ns.c
+++ bin/ch-run.ns.c
@@ -1,18 +1,24 @@
 /* Copyright © Los Alamos National Security, LLC, and others. */
 
 /* This program enters a UDSS and sets up partial isolation using namespaces.
-   The basic steps are:
+   By default, the basic steps are:
 
      1. Set up 3 of the 6 namespaces:
         * mount - filesystem isolation
         * IPC - SysV and POSIX shared memory, etc.
-        * user - so privilege escalation doesn't count
+        * user - so root within the container is unprivileged
      2. Call pivot_root(2) to enter the UDSS (similar to chroot).
-     3. Unmount the old root.
-     4. Drop privileges.
-     5. exec(2) the user program.
+     3. Unmount the old root filesystem.
+     4. exec(2) the user program.
 
-   Why not use other common container isolation mechanisms too?
+   There are preprocessor constants to alter this behavior, documented below.
+
+   Due to the user namespace, this program is not setuid root despite use of
+   privileged system calls. This eliminates the need to safely drop privileges
+   before invoking user code.
+
+   The following container isolation mechanisms are available, but we do not
+   use them:
 
      * UTS and network namespaces are omitted so the guest can use host
        network resources directly, without bridges and whatnot.
@@ -27,103 +33,97 @@
          http://man7.org/linux/man-pages/man7/pid_namespaces.7.html
          https://bugzilla.redhat.com/show_bug.cgi?id=894623
 
+       This introduces the need for a supervisor process to pass through
+       signals and perhaps other things. We want to avoid that.
+
      * cgroups are not needed because we assume single-tenancy, so there
-       aren't other jobs to mess up.
-
-   This program is designed to run setuid root. It will in fact refuse to run
-   if invoked directly by root, because this makes validation of no privilege
-   after dropping more complicated. (See PWN_ME_NOW below for an exception.)
-
-   Note that pivot_root(2) requires CAP_SYS_ADMIN, so there is no advantage to
-   running setcap rather than setuid.
-
-   Much of the logic in this program deals with dropping privileges safely,
-   which is a non-trivial task [e.g., 1]. We follow the recommendations in
-   [1], supplemented by [2, 3]. More specifically, we use the GNU extensions
-   setresuid(2) and setresgid(2) calls, which have a simpler API than the
-   POSIX setuid(2) and setgid(2), and we validate the lack of privilege after
-   dropping.
-
-   We do not worry about:
-
-     * Supplemental groups. Because we do not change the supplemental groups,
-       nothing needs to be dropped.
-
-     * Linux-specific fsuid and fsgid, which track euid/egid unless
-       specifically changed, which we do not. Kernel bugs have existed which
-       violate this invariant, but none are recent. Also, there are no
-       functions to query the fsuid and fsgid (you must read files in /proc).
-
-   This program requires Linux 3.10+ for user namespace support.
-
-   [1]: https://www.usenix.org/legacy/events/sec02/full_papers/chen/chen.pdf
-   [2]: https://www.securecoding.cert.org/confluence/display/c/POS36-C.+Observe+correct+revocation+order+while+relinquishing+privileges
-   [3]: https://www.securecoding.cert.org/confluence/display/c/POS37-C.+Ensure+that+privilege+relinquishment+is+successful
-
-*/
+       aren't other jobs to mess up. */
 
 #define _GNU_SOURCE
 #include <errno.h>
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mount.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
-void drop_privs();
-void efatal(const char * msg);
 void enter_udss();
-void fatal(const char * msg);
+void escalate();
+void fatal(const char * file, int line);
+void log_uids(const char * func, int line);
 void run_user_command(int argc, char * argv[]);
 void setup_namespaces();
+void try(int result);
 void usage();
-void verify_invoking_privs();
-void verify_no_privs();
 
 
 /** Constants **/
 
-/* The directory to which we chroot. This ought to provide some options,
-   though it's probably best to allow some selection from admin-defined
-   options rather than open-ended chrooting.
+/* There are three mount points in play for a successful pivot_root(2) into a
+   UDSS and unmount of the host filesystem.
 
-   The UDSS must already be mounted here, for two reasons. First,
-   loop-mounting a file requires a bunch of setup in userspace (i.e.,
-   mount(8)'s -o loop option is implemented by the mount program, not libc or
-   the system call). Second, the loop device would still have to be cleaned up
-   after we are done; at that point, we're running unprivileged and have
-   replaced ourselves with the user program. */
-#define CHROOT_TARGET "/chmnt"
+     1. newroot  This is the already-mounted UDSS filesystem we want to use.
+     2. target   Place to bind-mount newroot after CLONE_NEWUSER.
+     3. oldroot  Where the old root filesystem is mounted after pivot_root.
 
-/* Where to put the old (host) root on pivot. */
-#define CHROOT_PUT_OLD "/mnt/host"
+   target would seem to be unnecessary. However, CLONE_NEWUSER|CLONE_NEWNS
+   introduces some restrictions. In particular, pivot_root into a filesystem
+   that was mounted before CLONE_NEWUSER fails with EINVAL. One can work
+   around this by bind-mounting newroot to target before pivot_root.
 
-/* If set, don't drop privileges before running the user program. This mode is
-   grossly insecure. It is for testing what the user can do should he/she
-   escalate privileges. See verify_invoking_privs() for safety checks. Don't
-   change this file; instead, use the -D switch when compiling. */
-#ifdef NO_DROP_PRIVS
-  #warning "SECURITY: NO_DROP_PRIVS defined: Will run user programs as root"
+   Another way to work around this would be to do all UDSS mounting after
+   CLONE_NEWUSER. This requires setting up the loop device manually
+   (mount(8)'s -o loop option is implemented by the mount program, not libc or
+   the system call). This is not yet explored. In particular, teardown of the
+   loop device might be an issue, since by the time that's needed, we've
+   replaced ourselves with the user program. However, in theory, the loop
+   device ought to be local to the filesystem namespace. */
+#define NEWROOT "/chmnt"
+#define TARGET "/mnt"
+#define OLDROOT "/mnt"  // rooted at TARGET
+
+/* If set, do not set up the user namespace. This is for testing of other
+   security layers and should not be offered to normal users. It will make the
+   binary unusable except for root. */
+#ifdef NO_USER_NS
+  #warning "SECURITY: NO_USER_NS defined: Will not set up user namespace"
 #endif
+
+/* If set, run user program as root. In principle, this is safe, because
+   either (a) user namespace will protect the host or (b) escalation will fail
+   unless already privileged. However, some versions of Linux are buggy in
+   this regard, so this test setting should not be offered to normal users. */
+#ifdef RUN_AS_ROOT
+  #warning "SECURITY: RUN_AS_ROOT defined: Will run user programs as root"
+#endif
+
+/* If set, log current UIDs and capabilities at various points. */
+#define DEBUG_UIDS 1
+
+
+/** Macros **/
+
+/* Test the result of a system call: if not zero, exit with an error. This is
+   a macro so we have access to the file and line number. */
+#define TRY(x) if (x) fatal(__FILE__, __LINE__)
+
+/* Log the current UIDs. */
+#define LOG_UIDS log_uids(__func__, __LINE__)
+
 
 /** Main **/
 
 int main(int argc, char * argv[])
 {
-   verify_invoking_privs();
-   if (argc < 2) {
-#ifndef NO_DROP_PRIVS
-      drop_privs();
-      verify_no_privs();
-#endif
+   LOG_UIDS;
+   if (argc < 2)
       usage();
-   }
    setup_namespaces();
    enter_udss();
-#ifndef NO_DROP_PRIVS
-   drop_privs();
-   verify_no_privs();
+#ifdef RUN_AS_ROOT
+   escalate();
 #endif
    run_user_command(argc, argv);
 }
@@ -131,52 +131,54 @@ int main(int argc, char * argv[])
 
 /** Supporting functions **/
 
-/* Drop UID privileges and act as the invoking user. We should not have any
-   group privileges, so we do not drop them. We verify this lack of group
-   privilege later. */
-void drop_privs()
-{
-   uid_t ruid = getuid();
-   if (setresuid(ruid, ruid, ruid)) efatal("setresuid");
-}
-
-/* Report the string expansion of errno on stderr, then exit unsuccessfully. */
-void efatal(const char * msg)
-{
-   fprintf(stderr, "%s: %d: ", program_invocation_short_name, errno);
-   perror(msg);
-   exit(EXIT_FAILURE);
-}
-
 /* Enter the UDSS. After this, we are inside the UDSS and no longer have
    access to host resources except as provided. */
 void enter_udss(const char * image_path)
 {
    char * src, dst;
 
-   if (syscall(SYS_pivot_root, CHROOT_TARGET, CHROOT_TARGET CHROOT_PUT_OLD))
-      efatal("pivot_root");
-   if (chdir("/")) efatal("chdir(\"/\")");
-   if (umount2(CHROOT_PUT_OLD, MNT_DETACH)) efatal("umount2(CHROOT_PUT_OLD)");
+   LOG_UIDS;
+
+   TRY (mount(NEWROOT, TARGET, NULL, MS_BIND, NULL));
+   TRY (syscall(SYS_pivot_root, TARGET, TARGET OLDROOT));
+   TRY (chdir("/"));
+   TRY (umount2(OLDROOT, MNT_DETACH));
 
    // Mount the ancillary filesystems. We want these to be the guest versions,
    // not host ones.
-   if (mount(NULL, "/dev/shm", "tmpfs", MS_NOSUID | MS_NODEV, NULL))
-      efatal("mount(\"/dev/shm\")");
-   if (mount(NULL, "/proc", "proc", MS_NOSUID | MS_NODEV | MS_NOEXEC, NULL))
-      efatal("mount(\"/proc\")");
-   if (mount(NULL, "/run", "tmpfs", MS_NOSUID | MS_NODEV, NULL))
-      efatal("mount(\"/run\")");
-   if (mount(NULL, "/sys", "sysfs", MS_NOSUID | MS_NODEV | MS_NOEXEC, NULL))
-      efatal("mount(\"/sys\")");
+   TRY (mount(NULL, "/dev/shm", "tmpfs", MS_NOSUID | MS_NODEV, NULL));
+   TRY (mount(NULL, "/proc", "proc", MS_NOSUID | MS_NODEV | MS_NOEXEC, NULL));
+   TRY (mount(NULL, "/run", "tmpfs", MS_NOSUID | MS_NODEV, NULL));
+   TRY (mount(NULL, "/sys", "sysfs", MS_NOSUID | MS_NODEV | MS_NOEXEC, NULL));
    // FIXME: /dev/pts devpts, /dev/hugpages hugetblfs, /dev/mqueue mqueue
 }
 
-/* filesystems on stderr and exit unsuccessfully. */
-void fatal(const char * msg)
+/* Escalate to root. This requires CAP_SETUID, which we have if either (a) we
+   entered a user namespace or (b) the program was invoked by root, in which
+   case it's a no-op. That is, normal users can't use this to get root. */
+void escalate()
 {
-   fprintf(stderr, "%s: %d: %s\n", program_invocation_short_name, errno, msg);
+   LOG_UIDS;
+   TRY (setresuid(0, 0, 0));
+   LOG_UIDS;
+}
+
+/* Report the string expansion of errno on stderr, then exit unsuccessfully. */
+void fatal(const char * file, int line)
+{
+   fprintf(stderr, "%s:%d: %d: %s\n", file, line, errno, strerror(errno));
    exit(EXIT_FAILURE);
+}
+
+/* If DEBUG_UIDS, print uids on stderr prefixed with where. Otherwise, no-op. */
+void log_uids(const char * func, int line)
+{
+#ifdef DEBUG_UIDS
+   uid_t ruid, euid, suid;
+
+   TRY (getresuid(&ruid, &euid, &suid));
+   fprintf(stderr, "%s %d: uids=%d,%d,%d\n", func, line, ruid, euid, suid);
+#endif
 }
 
 /* Replace the current process with command and arguments starting at argv[1]
@@ -185,20 +187,26 @@ void fatal(const char * msg)
    execvp() requires null-termination instead of an argument count. */
 void run_user_command(int argc, char * argv[])
 {
+   LOG_UIDS;
    for (int i = 0; i < argc - 1; i++)
       argv[i] = argv[i + 1];
    argv[argc - 1] = NULL;
    execvp(argv[0], argv);  // only returns if error
-   efatal("execvp() failed");
+   fprintf(stderr, "%s: can't execute: %s", program_invocation_short_name,
+           strerror(errno));
 }
 
 /* Activate the desired isolation namespaces. */
 void setup_namespaces()
 {
-   // http://man7.org/linux/man-pages/man7/namespaces.7.html
-   if (unshare(CLONE_NEWIPC | CLONE_NEWNS))
-      efatal("unshare");
+   LOG_UIDS;
 
+   // http://man7.org/linux/man-pages/man7/namespaces.7.html
+   // http://man7.org/linux/man-pages/man7/user_namespaces.7.html
+   TRY (unshare(CLONE_NEWUSER | CLONE_NEWIPC | CLONE_NEWNS));
+   LOG_UIDS;
+
+#ifdef NO_USER_NS
    // Set all mounts to be slave mounts. The default on systemd systems is for
    // everything to be a shared mount. This has two effects. First, mounts and
    // unmounts inside the container propagate to the host. Second,
@@ -206,11 +214,13 @@ void setup_namespaces()
    // changing to slave mode, mounts and unmounts on the host will propagate
    // into the container, but not vice versa.
    //
+   // unshare(CLONE_NEWUSER) does this for us.
+   //
    // See e.g.:
    //   https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=739593
    //   https://www.kernel.org/doc/Documentation/filesystems/sharedsubtree.txt
-   if (mount(NULL, "/", NULL, MS_REC|MS_SLAVE, NULL))
-      efatal("mount(\"/\", MS_REC|MS_SLAVE)");
+   TRY (mount(NULL, "/", NULL, MS_REC|MS_SLAVE, NULL));
+#endif
 }
 
 /* Print a usage message and abort. */
@@ -218,69 +228,4 @@ void usage()
 {
    fprintf(stderr, "%s: usage: FIXME\n", program_invocation_short_name);
    exit(EXIT_FAILURE);
-}
-
-/* Verify that the UIDs and GIDs on invocation are as expected. If not, exit
-   with an error message. */
-void verify_invoking_privs()
-{
-   uid_t ruid, euid, suid;
-   gid_t rgid, egid, sgid;
-
-   if (getresuid(&ruid, &euid, &suid)) efatal("getresuid");
-   if (getresgid(&rgid, &egid, &sgid)) efatal("getresgid");
-
-/*
-   fprintf(stderr, "UIDs: real %u, effective %u, saved %u\n", ruid, euid, suid);
-   fprintf(stderr, "GIDs: real %u, effective %u, saved %u\n", rgid, egid, sgid);
-*/
-
-#ifdef NO_DROP_PRIVS
-
-   if (ruid != 0 || rgid != 0)
-      fatal("must run directly as root in NO_DROP_PRIVS mode (not setuid)");
-
-#else
-
-   if (ruid == 0)
-      fatal("must run as normal user, not root");
-   if (euid != 0 || suid != 0)
-      fatal("executable must be setuid root");
-
-   if (egid == 0)
-      fatal("will not run with GID 0");
-   if (rgid != egid || sgid != egid)
-      fatal("will not run setgid");
-
-#endif
-
-}
-
-/* Verify that we have no privileges. If not, exit with an error message. */
-void verify_no_privs()
-{
-   uid_t uid_wanted, ruid, euid, suid;
-   gid_t gid_wanted, rgid, egid, sgid;
-
-   // try to regain privileges; it should fail
-   if (setuid(0) != -1)
-      fatal("restoring privileges unexpectedly succeeded");
-
-   uid_wanted = getuid();
-   gid_wanted = getgid();
-
-   if (getresuid(&ruid, &euid, &suid) != 0) efatal("getresuid");
-   if (getresgid(&rgid, &egid, &sgid) != 0) efatal("getresgid");
-
-   // uids should be unprivileged and the same
-   if (uid_wanted == 0)
-      fatal("real UID unexpectedly 0");
-   if (uid_wanted != ruid || uid_wanted != euid || uid_wanted != suid)
-      fatal("inconsistent UID(s) found");
-
-   // gids should be unprivileged and the same
-   if (gid_wanted == 0)
-      fatal("real GID unexpectedly 0");
-   if (gid_wanted != rgid || gid_wanted != egid || gid_wanted != sgid)
-      fatal("inconsistent GID(s) found");
 }
