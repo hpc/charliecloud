f5098d39ee15666a96154d0cf0c2f97deb1c38da
Reid Priedhorsky <reidpr@lanl.gov>
2016-10-13 13:54:08 -0600

    couple Moab scripts

---
+++ examples/mpihello/moab.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run in directory where the tarball is.
+# Output will go to same place.
+
+# MSUB -l walltime=0:10:00
+
+set -e
+
+TAG=mpihello
+IMAGE=/tmp/$TAG
+
+printf 'host:      '
+mpirun --version | egrep '^mpirun'
+
+if [[ ! -e $IMAGE ]]; then
+    mpirun -pernode ch-tar2dir ./$USER.$TAG.tar.gz $IMAGE
+fi
+
+printf 'container: '
+ch-run $IMAGE -- mpirun --version | egrep '^mpirun'
+
+mpirun ch-run $IMAGE -- /hello/hello
