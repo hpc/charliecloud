69f438a1e83c6546819ddd9528ea5cca1e567a4c
Reid Priedhorsky <reidpr@lanl.gov>
2017-10-27 10:04:46 -0600

    document where /sys test files must reside (see #37)

--- test/chtest/dev_proc_sys.py
+++ test/chtest/dev_proc_sys.py
@@ -5,7 +5,12 @@ import sys
 
 # File in /sys seem to vary between Linux systems. Thus, try a few candidates
 # and use the first one that exists. What we want is any file under /sys with
-# permissions root:root -rw-------.
+# permissions root:root -rw------- that's in a directory readable and
+# executable by unprivileged users, so we know we're testing permissions on
+# the file rather than any of its containing directories. This may help:
+#
+#   $ find /sys -type f -a -perm 600 -ls
+#
 sys_file = None
 for f in ("/sys/devices/cpu/rdpmc",
           "/sys/kernel/mm/page_idle/bitmap",
