8d576176039ea3a59df4df2873d0a9415b344320
Rusty Davis <rustyd@lanl.gov>
2019-05-22 16:50:12 -0600

    Fix docker clean for buildah

--- test/docker-clean.sh
+++ test/docker-clean.sh
@@ -5,41 +5,43 @@
 set -e
 
 # Remove all containers.
-while true; do
-    cmd='sudo docker ps -aq'
-    cs_ct=$($cmd | wc -l)
-    echo "found $cs_ct containers"
-    [[ 0 -eq $cs_ct ]] && break
+buildah rm --all
+#while true; do
+    #cmd='sudo docker ps -aq'
+    #cs_ct=$($cmd | wc -l)
+    #echo "found $cs_ct containers"
+    # [[ 0 -eq $cs_ct ]] && break
     # shellcheck disable=SC2046
     #$sudo docker rm $($cmd)
-    buildah rm --all
-    buildah rmi --force --all
-done
+#done
 
 # Untag all images. This fails with:
 #
 #   Error response from daemon: invalid reference format
 #
 # sometimes. I don't know why.
-if [[ $1 != --all ]]; then
-    while true; do
-        cmd='sudo docker images --filter dangling=false --format {{.Repository}}:{{.Tag}}'
-        tag_ct=$($cmd | wc -l)
-        echo "found $tag_ct tagged images"
-        [[ 0 -eq $tag_ct ]] && break
-        # shellcheck disable=SC2046
-        #sudo docker rmi -f --no-prune $($cmd)
-    done
-fi
+# Can't currently figure out how to untag images in buildah
+#if [[ $1 != --all ]]; then
+#    while true; do
+#        #cmd='sudo docker images --filter dangling=false --format {{.Repository}}:{{.Tag}}'
+#        tag_ct=$($cmd | wc -l)
+#        echo "found $tag_ct tagged images"
+#        [[ 0 -eq $tag_ct ]] && break
+#        # shellcheck disable=SC2046
+#        #sudo docker rmi -f --no-prune $($cmd)
+#    done
+#fi
 
 # If --all specified, remove all images.
 if [[ $1 = --all ]]; then
-    while true; do
-        cmd='sudo docker images -q'
-        img_ct=$($cmd | wc -l)
-        echo "found $img_ct images"
-        [[ 0 -eq $img_ct ]] && break
+    buildah rm --all --force
+    #while true; do
+        #cmd='sudo docker images -q'
+        #img_ct=$($cmd | wc -l)
+        #echo "found $img_ct images"
+        #[[ 0 -eq $img_ct ]] && break
         # shellcheck disable=SC2046
         #sudo docker rmi -f $($cmd)
-    done
+        buildah rm --all --force
+    #done
 fi
