09fa26f4aa9068efcb74f689fb15d2ea71408f37
Rusty Davis <rustyd@lanl.gov>
2019-10-22 13:10:56 -0400

    Fix travis error

--- bin/ch-checkns.c
+++ bin/ch-checkns.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
    }
    if (argc >= 2 && strcmp(argv[1], "--help") == 0) {
       fprintf(stderr, usage);
-      return 1;
+      return 0;
    }
    if (argc >= 2 && strcmp(argv[1], "--version") == 0) {
       version();
