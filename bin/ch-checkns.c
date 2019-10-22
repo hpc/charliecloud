ebe140ce32601965370d61566a6b4538130db51e
Rusty Davis <rustyd@lanl.gov>
2019-10-22 13:33:59 -0400

    Checking test

--- bin/ch-checkns.c
+++ bin/ch-checkns.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 
    if (argc == 1) {
       fprintf(stderr, usage);
-      return 1;
+      return 0;
    }
    if (argc >= 2 && strcmp(argv[1], "--help") == 0) {
       fprintf(stderr, usage);
