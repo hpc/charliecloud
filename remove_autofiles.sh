72dc47704c16f8678b2374e738a1a51f6eb3ff9c
Rusty Davis <rustyd@lanl.gov>
2019-10-29 19:30:20 -0400

    Tests build

--- remove_autofiles.sh
+++ remove_autofiles.sh
@@ -1,1 +1,3 @@
 rm -rf Makefile.in aclocal.m4 autom4te.cache/ bin/Makefile.in config.h.in config.log  configure install-sh build-aux stamp-h1 config.status config.h
+rm -rf m4 libtools test/Makefile test/Makefile.in bin/Makefile
+rm -rf Makefile libtool
