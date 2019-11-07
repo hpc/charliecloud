54cbbef361f2bfd7ad0bfe8d8ae51b0971c368b5
Reid Priedhorsky <reidpr@lanl.gov>
2019-11-07 14:11:07 -0700

    [skip ci] build Python scripts

--- autogen.sh
+++ autogen.sh
@@ -23,6 +23,8 @@ rm -rf Makefile \
        aclocal.m4 \
        autom4te.cache \
        bin/.deps \
+       bin/Makefile \
+       bin/Makefile.in \
        bin/config.h \
        bin/config.h.in \
        bin/stamp-h1 \
@@ -37,3 +39,8 @@ if [[ $1 != --clean ]]; then
     autoheader
     autoreconf --install -Wall -Werror
 fi
+
+# Done.
+set +x
+echo
+echo 'Done. Next you should probably "./configure && make clean".'
