3a42924c0c02b572458270c0096f692a1ba1af8a
Reid Priedhorsky <1682574+reidpr@users.noreply.github.com>
2020-02-24 18:31:32 -0500

    PR #659 from me: autotools: add --with-sphinx-python, better docs (closes #611, closes #629)

--- autogen.sh
+++ autogen.sh
@@ -8,13 +8,29 @@ Usage:
 
   $ ./autogen.sh [--clean]
 
-Remove and rebuild Autotools files (./configure and friends).
+Remove and rebuild Autotools files (./configure and friends). This script is
+intended for developers; end users typically do not need it.
+
+Options:
 
   --clean  remove only; do not rebuild
 
 EOF
     exit 0
 fi
+
+cat <<EOF
+Removing and (maybe) rebuilding "configure" and friends.
+
+NOTE 1: This script is intended for developers. End users typically do not
+        need it.
+
+NOTE 2: Incomprehensible error messages about undefined macros can appear
+        below. This is usually caused by missing Autotools components.
+
+See the install instructions for details on both.
+
+EOF
 
 set -x
 
