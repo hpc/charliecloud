9927699e3c37abde9c4ac496305aa3da11a0ec50
Megan Rae Phinney <mphinney@gr-fe2.lanl.gov>
2020-07-02 09:59:23 -0600

    test.c

--- practice/test.c
+++ practice/test.c
@@ -1,14 +1,15 @@
-#include <squashfuse.h>
-#include <fuseprivate.h>
+#include "squashfuse.h"
+#include "fuseprivate.h"
 //#include <stat.h>
 #include "nonstd.h"
-#include <squashfuse_lib.h>
+#include "squashfuse_lib.h"
 
 #include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 int main(int argc, char *argv[]) {
 	struct fuse_args args;
@@ -25,7 +26,7 @@ int main(int argc, char *argv[]) {
 	memset(&sqfs_hl_ops, 0, sizeof(sqfs_hl_ops));
 	sqfs_hl_ops.init			= sqfs_hl_op_init;
 	sqfs_hl_ops.destroy		= sqfs_hl_op_destroy;
-	sqfs_hl_ops.getattr		= sqfs_hl_op_getattr;
+/*	sqfs_hl_ops.getattr		= sqfs_hl_op_getattr;
 	sqfs_hl_ops.opendir		= sqfs_hl_op_opendir;
 	sqfs_hl_ops.releasedir	= sqfs_hl_op_releasedir;
 	sqfs_hl_ops.readdir		= sqfs_hl_op_readdir;
@@ -56,7 +57,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	
 	fuse_opt_add_arg(&args, "-s"); /* single threaded */
-	ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
+	/*ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
 	fuse_opt_free_args(&args);
-	return ret;
+	return ret;*/
 }
