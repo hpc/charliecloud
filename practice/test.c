10d81f76f955de74cc64dc4c9884c1e0d7ca5e7a
Anna Chernikov <annaleezac@gmail.com>
2020-07-06 16:02:38 -0600

    the works

--- practice/test.c
+++ practice/test.c
@@ -23,10 +23,11 @@ int main(int argc, char *argv[]) {
 	};
 
 	struct fuse_operations sqfs_hl_ops;
-	memset(&sqfs_hl_ops, 0, sizeof(sqfs_hl_ops));
+        get_fuse_ops(&sqfs_hl_ops);
+	/**memset(&sqfs_hl_ops, 0, sizeof(sqfs_hl_ops));
 	sqfs_hl_ops.init			= sqfs_hl_op_init;
 	sqfs_hl_ops.destroy		= sqfs_hl_op_destroy;
-/*	sqfs_hl_ops.getattr		= sqfs_hl_op_getattr;
+	sqfs_hl_ops.getattr		= sqfs_hl_op_getattr;
 	sqfs_hl_ops.opendir		= sqfs_hl_op_opendir;
 	sqfs_hl_ops.releasedir	= sqfs_hl_op_releasedir;
 	sqfs_hl_ops.readdir		= sqfs_hl_op_readdir;
@@ -38,7 +39,7 @@ int main(int argc, char *argv[]) {
 	sqfs_hl_ops.listxattr	= sqfs_hl_op_listxattr;
 	sqfs_hl_ops.getxattr	= sqfs_hl_op_getxattr;
 	sqfs_hl_ops.statfs    = sqfs_hl_op_statfs;
-  
+        */
 	args.argc = argc;
 	args.argv = argv;
 	args.allocated = 0;
@@ -52,12 +53,12 @@ int main(int argc, char *argv[]) {
 	if (!opts.image)
 		sqfs_usage(argv[0], true);
 	
-	hl = sqfs_hl_open(opts.image, opts.offset);
+	hl =sqfs_hl_open(opts.image, opts.offset);
 	if (!hl)
 		return -1;
 	
 	fuse_opt_add_arg(&args, "-s"); /* single threaded */
-	/*ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
+	ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
 	fuse_opt_free_args(&args);
-	return ret;*/
+	return ret;
 }
