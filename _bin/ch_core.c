d9e5de36af7437625bcf4b183117a4fa6a0b633e
Megan Rae Phinney <mphinney@wc-fe1.lanl.gov>
2020-07-07 13:39:51 -0600

    started add mounting to ch-run

--- _bin/ch_core.c
+++ _bin/ch_core.c
@@ -21,7 +21,7 @@
 #include "config.h"
 #include "ch_misc.h"
 #include "ch_core.h"
-
+#include "ops.h"
 
 /** Macros **/
 
@@ -461,4 +461,31 @@ void tmpfs_mount(const char *dst, const char *newroot, const char *data)
 
    Zf (mount(NULL, dst_full, "tmpfs", 0, data),
        "can't mount tmpfs at %s", dst_full);
+}
+
+/*mount sqfs*/
+int squashmount(/*int argc,*/ char *argv[]) 
+{
+   //fuse args struct
+  struct fuse_args args;
+  //sqfs struct
+  sqfs_hl *hl;
+  //return value
+  int ret;
+
+  //get fuse operations struct from external libraries
+  struct fuse_operations sqfs_hl_ops;
+  get_fuse_ops(&sqfs_hl_ops);
+  //create sqfs struct with path to image, and offset
+  hl =sqfs_hl_open(argv[1], 0);
+  if (!hl)
+    return -1;
+
+  //pass in arguments to fuse main containing program name, mount location, single threaded option
+  fuse_opt_add_arg(&args, argv[0]);
+  fuse_opt_add_arg(&args, "/var/tmp/anna");
+  fuse_opt_add_arg(&args, "-s"); /* single threaded */
+  ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
+  fuse_opt_free_args(&args);
+  return ret;
 }
---
+++ _bin/config.h
@@ -0,0 +1,42 @@
+/* bin/config.h.  Generated from config.h.in by configure.  */
+/* bin/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `asprintf' function. */
+#define HAVE_ASPRINTF 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+/* #undef HAVE_PTHREAD_PRIO_INHERIT */
+
+/* user+mount namespaces work */
+#define HAVE_USERNS 1
+
+/* Name of package */
+#define PACKAGE "charliecloud"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/hpc/charliecloud"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Charliecloud"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Charliecloud 0.17~pre+fuse.ca7b3e2.dirty"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "charliecloud"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.17~pre+fuse.ca7b3e2.dirty"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Version number of package */
+#define VERSION "0.17~pre+fuse.ca7b3e2.dirty"
