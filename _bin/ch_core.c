518ee08186a9c4138225e57858f8e5b8acbe0d7a
Megan Rae Phinney <mphinney@wc-fe1.lanl.gov>
2020-07-07 14:56:40 -0600

    updated squashmount() method

--- _bin/ch_core.c
+++ _bin/ch_core.c
@@ -463,30 +463,40 @@ void tmpfs_mount(const char *dst, const char *newroot, const char *data)
        "can't mount tmpfs at %s", dst_full);
 }
 
-/*mount sqfs*/
-int squashmount(/*int argc,*/ char *argv) 
+int squashmount(char *argv)
 {
- printf("%s\n", argv); 
-  //fuse args struct
-  struct fuse_args args;
-  //sqfs struct
-  sqfs_hl *hl;
-  //return value
-  int ret;
+	//fuse args struct
+	struct fuse_args args;
+	//sqfs struct
+	sqfs_hl *hl;
+	//return val
+	int ret;
 
-  //get fuse operations struct from external libraries
-  struct fuse_operations sqfs_hl_ops;
-  get_fuse_ops(&sqfs_hl_ops);
-  //create sqfs struct with path to image, and offset
-  hl =sqfs_hl_open(argv, 0);
-  if (!hl)
-    return -1;
+	//get fuse ops struct frome external libraries
+	struct fuse_operations sqfs_hl_ops;
+	get_fuse_ops(&sqfs_hl_ops);
 
-  //pass in arguments to fuse main containing program name, mount location, single threaded option
-  fuse_opt_add_arg(&args, argv);
-  fuse_opt_add_arg(&args, "/var/tmp/megan");
-  fuse_opt_add_arg(&args, "-s"); /* single threaded */
-  ret = fuse_main(args.argc, args.argv, &sqfs_hl_ops, hl);
-  fuse_opt_free_args(&args);
-  return ret;
+	//create sqfs struct w/ path to image
+	hl = sqfs_hl_open(argv, 0); //have to change to path??
+	if(!hl)
+		return -1;
+	
+	//make dir to mount
+	char *name = strtok(basename("/var/tmp/megan",".");
+	char *buffer = (char *) malloc(strlen(name) + 10);
+	strcopy(buffer, "var/tmp/");
+	char *mountdir = strcat(buffer, name);
+
+	if(mkdir(mountdir, 077) != 0)
+		return -1;
+
+	//PASS IN ARGUMENTS TO FUSE MAIN CONTAINING program name, mount locations, single threaded option
+	fuse_opt_add_args(&args, argv[0]);
+	fuse_opt_add_args(&args, mountdir);
+	fuse_opt_add_args(&args, "-s");
+	ret = fuse_main(args.argc, args,argv, &sqfs_hl_ops, hl);
+	fuse_opt_free_args(&args);
+	return ret;
+	
 }
+
