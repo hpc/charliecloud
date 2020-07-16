c3d0d8091661d61d4ee3ec4ab481cfa5db46c56f
Anna Chernikov <annaleezac@gmail.com>
2020-07-16 15:51:48 -0600

    removing our practice directory

--- practice/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./test ~/chorkshop/hello.sqfs
-mount | grep -F fuse
-ch-run /var/tmp/hello -- ./hello.py
-fusermount -u /var/tmp/hello
-mount | grep -F fuse
-rm -rf /var/tmp/hello
--- practice/test2.c
+++
@@ -1,4 +0,0 @@
-#include <ops.h>
-int main(){
-   hello_world();
-}
