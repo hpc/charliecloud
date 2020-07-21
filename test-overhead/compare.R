402a3ee24dc4576b3d9b1c25f7dca3b4aad7bb07
Anna Chernikov <annaleezac@gmail.com>
2020-07-21 13:12:04 -0600

    finished testing ex02

--- test-overhead/compare.R
+++ test-overhead/compare.R
@@ -1,16 +1,57 @@
 
 
 e2e <- read.csv('ex02-E2E.csv')
+broken <- read.csv('ex02.csv')
 
-print("Average End to End Runtime for Test Group 1:")
+cat("Average and Std Dev End to End Runtime for Test Group 1:\n")
 mean(e2e$E2E.TG1)
+sd(e2e$E2E.TG1)
 
-print("Average End to End Runtime for Test Group 2:")
+cat("Average and Std Dev End to End Runtime for Test Group 2:\n")
 mean(e2e$E2E.TG2)
+sd(e2e$E2E.TG2)
 
-print("Average End to End Runtime for Test Group 3:")
+cat("Average and Std Dev End to End Runtime for Test Group 3:\n")
 mean(e2e$E2E.TG3)
+sd(e2e$E2E.TG3)
 
-
-print("TTest between test groups 2 and 3")
+cat("TTest between test groups 2 and 3:\n")
 t.test(e2e$E2E.TG3, e2e$E2E.TG2)
+
+
+cat("Average Mount Time for Test group 2:\n")
+mean(broken$MT.TG2)
+sd(broken$MT.TG2)
+
+cat("Average Mount time for Test group 3:\n")
+mean(broken$MT.TG3)
+sd(broken$MT.TG3)
+
+cat("Ttest between mount times for test groups 2 and 3:\n")
+t.test(broken$MT.TG3, broken$MT.TG2)
+
+cat("Average Run Time for Test group 2:\n")
+mean(broken$RT.TG2)
+sd(broken$RT.TG2)
+
+cat("Average Run time for Test group 3:\n")
+mean(broken$RT.TG3)
+sd(broken$RT.TG3)
+
+cat("Ttest between run times for test groups 2 and 3:\n")
+t.test(broken$RT.TG3, broken$RT.TG2)
+
+
+cat("Average UnMount Time for Test group 2:\n")
+mean(broken$UT.TG2)
+sd(broken$UT.TG2)
+
+cat("Average Unmount time for Test group 3:\n")
+mean(broken$UT.TG3)
+sd(broken$UT.TG3)
+
+cat("Ttest between unmount times for test groups 2 and 3:\n")
+t.test(broken$UT.TG3, broken$UT.TG2)
+
+
+
--- test-overhead/ex02.sh
+++ test-overhead/ex02.sh
@@ -2,7 +2,7 @@
 
 
 #create schema
-printf "ID, E2E-TG1, E2E-TG2, E2E-TG3\n" > ex02-E2E.csv
+#printf "ID, E2E-TG1, E2E-TG2, E2E-TG3\n" > ex02-E2E.csv
 
 
 UnpackedTar="$HOME/hello"
@@ -11,11 +11,11 @@ Squash=$HOME/chorkshop/hello.sqfs
 
 
 #repeat 1000 times
-for i in {1..1000}
+for i in {1..0}
 do
 
 #Write ID
-printf "$i," >> ex02-E2E.csv
+#printf "$i," >> ex02-E2E.csv
 
 #Time E2E for each Test Group
 E2ETG1=$((time sh -c 'ch-run ~/hello -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
@@ -31,7 +31,7 @@ E2ETG3=$((time sh -c '~/charliecloud/bin/ch-run ~/chorkshop/hello.sqfs -- ./hell
 
 
 
-printf "$E2ETG1, $E2ETG2, $E2ETG3\n" >> ex02-E2E.csv
+#printf "$E2ETG1, $E2ETG2, $E2ETG3\n" >> ex02-E2E.csv
 
 done
 
@@ -41,7 +41,7 @@ done
 
 printf "ID, MT-TG2, MT-TG3, UT-TG2, UT-TG3, RT-TG2, RT-TG3\n" > ex02.csv
 
-for i in {1..1}
+for i in {1..1000}
 do
 
 printf "$i," >> ex02.csv
@@ -60,10 +60,9 @@ UTTG2=$((time sh -c 'ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{prin
 
 
 out=$(sh -c '~/charliecloud/bin/ch-run ~/chorkshop/hello.sqfs -- ./hello.py')
-
-MTTG3=$out | grep -F mount | awk '{print $2}'
-RTTG3=$out | grep -F run | awk '{print $2}'
-UTTG3=$out | grep -F unmount | awk '{print $2}'
+MTTG3=$((echo $out) | awk '{print $4}')
+RTTG3=$((echo $out) | awk '{print $6}')
+UTTG3=$((echo $out) | awk '{print $8}')
 
 printf "$MTTG2, $MTTG3, $UTTG2, $UTTG3, $RTTG2,$RTTG3\n" >> ex02.csv
 done 
