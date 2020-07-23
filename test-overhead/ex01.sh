780595f20e6f1ec02f16fef80a3da72b1f529e0b
Anna Chernikov <annaleezac@gmail.com>
2020-07-23 15:03:57 -0600

    started outline for experiment one

--- test-overhead/ex01.sh
+++ test-overhead/ex01.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-printf "ID, Size, Runtime G1, Runtime G2\n" > out.csv
+printf "ID, Size, E2E-TG2A, E2E-TG2B, E2E-TG3\n" > ex01-E2E.csv
 
 
 
+while IFS=, read -r bytes file
 
-for j in {1..3}
 do
-for i in {1..10}
-do
-   
-   printf "$i," >> out.csv
-   Size=wc -c $HOME/chorkshop/hello.sqfs | awk '{print $1}'
-   printf "$Size," >> out.csv
-   truncate -s + 10000000 $HOME/chorkshop/hello.sqfs 
 
-   G1=time ch-mount $HOME/chorkshop/hello.sqfs /var/tmp && ch-run /var/tmp/hello -- ./hello.py && fusermount -u /var/tmp/hello
-   G2=time ch-run $HOME/chorkshop/hello.sqfs -- ./hello.py
-   printf "$G1,$G2\n" >> out.csv
+#echo "I got:$bytes|$file"
+#100 trials per file size
+for i in {1..5}
+do
+
+printf "$i, $bytes," >> ex01-E2E.csv
+
+
+#Ch-Mount High Level Workflow: end to end test group 2
+E2ETG2A=$((time sh -c '~/charliecloud/bin/ch-mounthl ~/chorkshop/"$file" /var/tmp && ch-run /var/tmp/hello -- ./hello.py && ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+
+
+#Ch-mount low level workflow: end to end test group 2
+E2ETG2B=$((time sh -c '~/charliecloud/bin/ch-mount ~/chorkshop/"$file" /var/tmp && ch-run /var/tmp/hello -- ./hello.py && ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+
+
+#New automount workflow: end to end test group 3
+E2ETG3=$((time sh -c '~/charliecloud/bin/ch-run ~/chorkshop/"$file" -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+
+
+
+#printf "$E2ETG2A, $E2ETG2B, $E2ETG3\n" >> ex02-E2E.csv
+
 done
-done
+
+
+done < ex01-files.csv
+
