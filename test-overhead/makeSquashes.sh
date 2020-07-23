1ff0eb625885e4041c9829525b0f132a623aed01
Anna Chernikov <annaleezac@gmail.com>
2020-07-23 11:05:22 -0400

    added script to generate test sqfs

---
+++ test-overhead/makeSquashes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+printf "ID, Bytes, Filename\n" > ex01-files.csv
+
+
+unsquashfs hello.sqfs 
+
+for i in {0..0}
+do
+
+	var=$((2**"$i"))
+	dd if=/dev/urandom of=ugh.txt bs="$var"M count=1  
+	mv ugh.txt squashfs-root/
+	filename=hello"$var".sqfs
+	mksquashfs squashfs-root "$filename"
+
+	
+
+	Bytes=$(ls -l "$filename" | awk '{print $5}')
+        printf "$i, $Bytes, $filename\n" >> ex01-files.csv
+	
+
+
+done
+rm -rf squashfs-root
+
