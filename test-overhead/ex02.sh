94fcaf56eeb1ffb7d9d01812368a1221a26631b6
Anna Chernikov <annaleezac@gmail.com>
2020-07-24 15:50:57 -0600

    cleaned up experiment 2

--- test-overhead/ex02.sh
+++ test-overhead/ex02.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
 
-#create schema
-#printf "ID, E2E-TG1, E2E-TG2A, E2E-TG2B, E2E-TG3\n" > ex02-E2E.csv
 
 
+########## VARIABLES ############################################
+
+CHMOUNTHL=$HOME/charliecloud/bin/ch-mounthl
+CHMOUNTLL=$HOME/charliecloud/bin/ch-mount
+CHUMOUNT=$HOME/charliecloud/bin/ch-umount
+CHRUN=$HOME/charliecloud/bin/ch-run
+CHTAR2DIR=$HOME/charliecloud/bin/ch-tar2dir
+
+SQFS=$HOME/chorkshop/hello.sqfs
+TAR=$HOME/hello.tar.gz
+
+#################################################################
+# End to End Workflow just for giggles (giggles.csv)            #
+#################################################################
 
 
-#TEST THE END TO END WORKFLOW FOR ALL TEST GROUPS AND PIPE TO ex02-E2E.csv
-#1000 trials
-for i in {1..0}
+printf "ID, E2E-TB, E2E-SFSH, E2E-SFSL, E2E-PSFSH\n" > giggles.csv
+
+for i in {1..30}
 do
+    start=`date '+%s.%N'`
+    #Suggested SquashfS High Level Workflow
+    $CHMOUNTHL $SQFS /var/tmp && $CHRUN /var/tmp/hello -- ./hello.py && $CHUMOUNT /var/tmp/hello
 
-#Unpacked Tar Ball Workflow: end to end test group 1
-E2ETG1=$((time sh -c 'ch-run ~/hello -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    end1=`date '+%s.%N'`
 
+    #Suggested SquashFS Low Level Workflow
+    $CHMOUNTLL $SQFS /var/tmp && $CHRUN /var/tmp/hello -- ./hello.py && $CHUMOUNT /var/tmp/hello
+    end2=`date '+%s.%N'`
 
+    #Proposed SquashFS High Level Workflow
+    $CHRUN $SQFS -- ./hello.py
+    end3=`date '+%s.%N'`
 
-#Ch-Mount High Level Workflow: end to end test group 2
-E2ETG2A=$((time sh -c '~/charliecloud/bin/ch-mounthl ~/chorkshop/hello.sqfs /var/tmp && ch-run /var/tmp/hello -- ./hello.py && ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    #Tar Ball
+    $CHTAR2DIR $TAR /tmp && $CHRUN /tmp/hello -- ./hello.py 
+    end4=`date '+%s.%N'`
 
+    rm -rf /tmp/hello
 
-#Ch-mount low level workflow: end to end test group 2
-E2ETG2B=$((time sh -c '~/charliecloud/bin/ch-mount ~/chorkshop/hello.sqfs /var/tmp && ch-run /var/tmp/hello -- ./hello.py && ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    E2ESFSH=$(echo "$end1" - "$start" | bc -l)
 
+    E2ESFSL=$(echo "$end2" - "$end1" | bc -l)
 
-#New automount workflow: end to end test group 3
-E2ETG3=$((time sh -c '~/charliecloud/bin/ch-run ~/chorkshop/hello.sqfs -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    E2EPSFSH=$(echo "$end3" - "$end2" | bc -l)
 
+    E2ETB=$(echo "$end4" - "$end3" | bc -l)
 
+    printf "$i, $E2ETB, $E2ESFSH, $E2ESFSL, $E2EPSFSH\n" >> giggles.csv
 
-#printf "$i, $E2ETG1, $E2ETG2A, $E2ETG2B, $E2ETG3\n" >> ex02-E2E.csv
 
 done
 
@@ -38,65 +60,133 @@ done
 
 
 
-printf "ID, MT-TG2A, MT-TG2B, MT-TG3, UT-TG2A, UT-TG2B,  UT-TG3, RT-TG2A, RT-TG2B, RT-TG3\n" > ex02.csv
+################################################################
+# Full workflow(ex02-E2E.csv)                                  #
+################################################################
 
-#TEST BROKEN UP WORKFLOW FOR ALL TEST GROUPS AND PIPE TO EX02.CSV
+
+#create schema
+printf "ID, E2E-SFSH, E2E-SFSL, E2E-PSFSH\n" > ex02-E2E.csv
+
+#1000 trials
+for i in {1..1000}
+do
+    start=`date '+%s.%N'`
+    #Suggested SquashfS High Level Workflow
+    $CHMOUNTHL $SQFS /var/tmp && $CHRUN /var/tmp/hello -- ./hello.py && $CHUMOUNT /var/tmp/hello
+
+    end1=`date '+%s.%N'`
+
+    #Suggested SquashFS Low Level Workflow
+    $CHMOUNTLL $SQFS /var/tmp && $CHRUN /var/tmp/hello -- ./hello.py && $CHUMOUNT /var/tmp/hello
+    end2=`date '+%s.%N'`
+
+    #Proposed SquashFS High Level Workflow
+    $CHRUN $SQFS -- ./hello.py
+    end3=`date '+%s.%N'`
+
+    E2ESFSH=$(echo "$end1" - "$start" | bc -l)
+
+    E2ESFSL=$(echo "$end2" - "$end1" | bc -l)
+
+    E2EPSFSH=$(echo "$end3" - "$end2" | bc -l)
+
+    printf "$i, $E2ESFSH, $E2ESFSL, $E2EPSFSH\n" >> ex02-E2E.csv
+
+done
+
+
+###################################################################
+# Broken Workflow (ex02.csv)                                      #
+###################################################################
+
+
+
+printf "ID, MT-SFSH, MT-SFSL, MT-PSFSH, UT-SFSH, UT-SFSL,  UT-PSFSH, RT-SFSH, RT-SFSL, RT-PSFSH, RT-TB\n" > ex02.csv
+
+$CHTAR2DIR $TAR /tmp/
+
 for i in {1..1000}
 do
 
-printf "$i," >> ex02.csv
-MTTG2B=$((time sh -c '~/charliecloud/bin/ch-mount ~/chorkshop/hello.sqfs /var/tmp') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    #Suggested SquashFS Low Level Workflow
+
+    start=`date '+%s.%N'`
+    $CHMOUNTLL $SQFS /var/tmp
+    end1=`date '+%s.%N'`
+
+    $CHRUN /var/tmp/hello -- ./hello.py
+    end2=`date '+%s.%N'`
+
+    $CHUMOUNT /var/tmp/hello
+    end3=`date '+%s.%N'`
+
+    MTSFSL=$(echo "$end1" - "$start" | bc -l)
+
+    RTSFSL=$(echo "$end2" - "$end1" | bc -l)
+
+    UTSFSL=$(echo "$end3" - "$end2" | bc -l)
 
 
-RTTG2B=$((time sh -c 'ch-run /var/tmp/hello -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    #Suggested SquashfS High Level Workflow
+    
+    start=`date '+%s.%N'`
+    $CHMOUNTHL $SQFS /var/tmp
+    end1=`date '+%s.%N'`
+
+    $CHRUN /var/tmp/hello -- ./hello.py
+    end2=`date '+%s.%N'`
+
+    $CHUMOUNT /var/tmp/hello
+    end3=`date '+%s.%N'`
+
+    MTSFSH=$(echo "$end1" - "$start" | bc -l)
+
+    RTSFSH=$(echo "$end2" - "$end1" | bc -l)
+
+    UTSFSH=$(echo "$end3" - "$end2" | bc -l)
 
 
+    #Proposed SquashFS High Level Workflow
 
-UTTG2B=$((time sh -c 'ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
+    rm -rf out.txt
+    adddate() {
+        while IFS= read -r line; do
+            printf '%s %s\n' "$(date '+%s.%N')" "$line" >> out.txt;
+        done
+    }
+    start=`date '+%s.%N'`
 
-MTTG2A=$((time sh -c '~/charliecloud/bin/ch-mounthl ~/chorkshop/hello.sqfs /var/tmp') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
-
-
-RTTG2A=$((time sh -c 'ch-run /var/tmp/hello -- ./hello.py') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
-
-
-
-UTTG2A=$((time sh -c 'ch-umount /var/tmp/hello') 2>&1 | grep -F real | awk '{print $2}' | grep -Eo '[+]?([.][0-9]+)?')
-
-
-
-rm -rf out.txt
-
-adddate() {
-    while IFS= read -r line; do
-        printf '%s %s\n' "$(date +%s.%N)" "$line" >> out.txt;
-    done
-}
-
-start=`date +%s.%N`
-
-sh -c '~/charliecloud/bin/ch-run ~/chorkshop/hello.sqfs -- ./hello.py' |& adddate 
+    $CHRUN $SQFS -- ./hello.py |& adddate 
 
 
-end1=$(cat out.txt | grep -w "mount" | awk '{printf $1}')
+    end1=$(cat out.txt | grep -w "mount" | awk '{printf $1}')
 
-end2=$(cat out.txt | grep -F "run" | awk '{printf $1}')
+    end2=$(cat out.txt | grep -F "run" | awk '{printf $1}')
 
-end3=$(cat out.txt | grep -m2 "unmount" | tail -n1 |  awk '{printf $1}')
-
-
-MTTG3=$(echo "$end1" - "$start" | bc -l)
-
-RTTG3=$(echo "$end2" - "$end1" | bc -l)
-
-UTTG3=$(echo "$end3" - "$end2" | bc -l)
+    end3=$(cat out.txt | grep -m2 "unmount" | tail -n1 |  awk '{printf $1}')
 
 
+    MTPSFSH=$(echo "$end1" - "$start" | bc -l)
 
-printf "$MTTG2A,$MTTG2B, $MTTG3, $UTTG2A, $UTTG2B, $UTTG3, $RTTG2A,$RTTG2B,$RTTG3\n" >> ex02.csv
+    RTPSFSH=$(echo "$end2" - "$end1" | bc -l)
+
+    UTPSFSH=$(echo "$end3" - "$end2" | bc -l)
+
+
+    #Tar Ball Workflow (Just Runtime)
+    
+    starti=`date '+%s.%N'`    
+    $CHRUN /tmp/hello -- ./hello.py
+    end4=`date '+%s.%N'`
+
+    RTTB=$(echo "$end4" - "$starti" | bc -l)
+    echo $RTTB
+
+    printf "$i,$MTSFSH,$MTSFSL, $MTPSFSH, $UTSFSH, $UTSFSL, $UTPSFSH, $RTSFSL,$RTSFSH,$RTPSFSH,$RTTB\n" >> ex02.csv
 done 
 
-
+rm -rf /tmp/hello
 
 
 
