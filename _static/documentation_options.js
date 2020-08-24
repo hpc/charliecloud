adbf3511f95820f377ada7b8b9e9e02c0c1eb5d2
Hunter Easterday <heasterday@lanl.gov>
2020-08-24 17:18:24 -0600

    docs for commit 06e48a1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18',
+    VERSION: '0.19~pre+fixsearch.06e48a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
