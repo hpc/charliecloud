ce01fd9a6b05e8abd37b730784fae870ff7110d9
Reid Priedhorsky <1682574+reidpr@users.noreply.github.com>
2021-02-05 13:03:26 -0700

    PR #960: keep metadata during pull and FROM (closes #651, closes #765)

--- lib/build.py
+++ lib/build.py
@@ -58,8 +58,6 @@ ARG_DEFAULTS = { "HTTP_PROXY": os.environ.get("HTTP_PROXY"),
                  # chown(2) and chgrp(2) files to whatever's in the tarball.
                  "TAR_OPTIONS": "--no-same-owner" }
 
-ENV_DEFAULTS = { }
-
 
 ## Main ##
 
@@ -136,9 +134,9 @@ def main(cli_):
    try:
       tree = parser.parse(text)
    except lark.exceptions.UnexpectedInput as x:
-      ch.DEBUG(x)  # noise about what was expected in the grammar
+      ch.VERBOSE(x)  # noise about what was expected in the grammar
       ch.FATAL("can't parse: %s:%d,%d\n\n%s" % (cli.file, x.line, x.column, x.get_context(text, 39)))
-   ch.DEBUG(tree.pretty())
+   ch.VERBOSE(tree.pretty())
 
    # Sometimes we exit after parsing.
    if (cli.parse_only):
@@ -207,6 +205,8 @@ class Main_Loop(lark.Visitor):
             else:
                ch.FATAL("first instruction must be ARG or FROM")
          inst.execute()
+         if (image_i != -1):
+            images[image_i].metadata_save()
          self.instruction_ct += inst.execute_increment
 
 
@@ -380,7 +380,7 @@ class I_copy(Instruction):
       dst = ch.Path(dst)
       assert (os.path.isdir(src) and not os.path.islink(src))
       assert (os.path.isdir(dst) and not os.path.islink(dst))
-      ch.DEBUG("copying named directory: %s -> %s" % (src, dst), v=2)
+      ch.DEBUG("copying named directory: %s -> %s" % (src, dst))
       for (dirpath, dirnames, filenames) in os.walk(src, onerror=onerror):
          dirpath = ch.Path(dirpath)
          subdir = dirpath.relative_to(src)
@@ -393,23 +393,23 @@ class I_copy(Instruction):
             d = ch.Path(d)
             src_path = dirpath // d
             dst_path = dst_dir // d
-            ch.DEBUG("dir: %s -> %s" % (src_path, dst_path), v=2)
+            ch.TRACE("dir: %s -> %s" % (src_path, dst_path))
             if (os.path.islink(src_path)):
                filenames.append(d)  # symlink, handle as file
-               ch.DEBUG("symlink to dir, will handle as file", v=2)
+               ch.TRACE("symlink to dir, will handle as file")
                continue
             else:
                dirnames.append(d)   # directory, descend into later
             # If destination exists, but isn't a directory, remove it.
             if (os.path.exists(dst_path)):
                if (os.path.isdir(dst_path) and not os.path.islink(dst_path)):
-                  ch.DEBUG("dst_path exists and is a directory", v=2)
+                  ch.TRACE("dst_path exists and is a directory")
                else:
-                  ch.DEBUG("dst_path exists, not a directory, removing", v=2)
+                  ch.TRACE("dst_path exists, not a directory, removing")
                   ch.unlink(dst_path)
             # If destination directory doesn't exist, create it.
             if (not os.path.exists(dst_path)):
-               ch.DEBUG("mkdir dst_path", v=2)
+               ch.TRACE("mkdir dst_path")
                ch.ossafe(os.mkdir, "can't mkdir: %s" % dst_path, dst_path)
             # Copy metadata, now that we know the destination exists and is a
             # directory.
@@ -420,12 +420,12 @@ class I_copy(Instruction):
             f = ch.Path(f)
             src_path = dirpath // f
             dst_path = dst_dir // f
-            ch.DEBUG("file or symlink via copy2: %s -> %s"
-                     % (src_path, dst_path), v=2)
+            ch.TRACE("file or symlink via copy2: %s -> %s"
+                      % (src_path, dst_path))
             if (not (os.path.isfile(src_path) or os.path.islink(src_path))):
                ch.FATAL("can't COPY: unknown file type: %s" % src_path)
             if (os.path.exists(dst_path)):
-               ch.DEBUG("destination exists, removing", v=2)
+               ch.TRACE("destination exists, removing")
                if (os.path.isdir(dst_path) and not os.path.islink(dst_path)):
                   ch.rmtree(dst_path)
                else:
@@ -443,7 +443,7 @@ class I_copy(Instruction):
       assert (   not os.path.exists(dst)
               or (os.path.isdir(dst) and not os.path.islink(dst))
               or (os.path.isfile(dst) and not os.path.islink(dst)))
-      ch.DEBUG("copying named file: %s -> %s" % (src, dst), v=2)
+      ch.DEBUG("copying named file: %s -> %s" % (src, dst))
       ch.copy2(src, dst, follow_symlinks=True)
 
    def dest_realpath(self, unpack_path, dst):
@@ -460,26 +460,26 @@ class I_copy(Instruction):
          iter_ct += 1
          if (iter_ct > 100):  # arbitrary
             ch.FATAL("can't COPY: too many path components")
-         ch.DEBUG("current destination: %d %s" % (iter_ct, dst_canon), v=2)
-         #ch.DEBUG("parts remaining: %s" % dst_parts, v=2)
+         ch.TRACE("current destination: %d %s" % (iter_ct, dst_canon))
+         #ch.TRACE("parts remaining: %s" % dst_parts)
          part = dst_parts.pop()
          if (part == "/" or part == "//"):  # 3 or more slashes yields "/"
-            ch.DEBUG("skipping root")
+            ch.TRACE("skipping root")
             continue
          cand = dst_canon // part
-         ch.DEBUG("checking: %s" % cand, v=2)
+         ch.TRACE("checking: %s" % cand)
          if (not cand.is_symlink()):
-            ch.DEBUG("not symlink", v=2)
+            ch.TRACE("not symlink")
             dst_canon = cand
          else:
             target = ch.Path(os.readlink(cand))
-            ch.DEBUG("symlink to: %s" % target, v=2)
+            ch.TRACE("symlink to: %s" % target)
             assert (len(target.parts) > 0)  # POSIX says no empty symlinks
             if (target.is_absolute()):
-               ch.DEBUG("absolute")
+               ch.TRACE("absolute")
                dst_canon = ch.Path(unpack_path)
             else:
-               ch.DEBUG("relative", v=2)
+               ch.TRACE("relative")
             dst_parts.extend(reversed(target.parts))
       return dst_canon
 
@@ -508,13 +508,13 @@ class I_copy(Instruction):
                ch.FATAL("COPY --from: stage %s does not exist" % self.from_)
          context = images[self.from_].unpack_path
       context_canon = os.path.realpath(context)
-      ch.DEBUG("context: %s" % context)
+      ch.VERBOSE("context: %s" % context)
       # Expand source wildcards.
       srcs = list()
       for src in self.srcs:
          for i in glob.glob("%s/%s" % (context, src)):  # glob can't take Path
             srcs.append(i)
-            ch.DEBUG("source: %s" % i)
+            ch.VERBOSE("source: %s" % i)
       if (len(srcs) == 0):
          ch.FATAL("can't COPY: no sources found")
       # Validate sources are within context directory. (Can't convert to
@@ -530,9 +530,9 @@ class I_copy(Instruction):
          dst = ch.Path(self.dst)
       else:
          dst = env.workdir // self.dst
-      ch.DEBUG("destination, as given: %s" % dst)
+      ch.VERBOSE("destination, as given: %s" % dst)
       dst_canon = self.dest_realpath(unpack_canon, dst) # strips trailing slash
-      ch.DEBUG("destination, canonical: %s" % dst_canon)
+      ch.VERBOSE("destination, canonical: %s" % dst_canon)
       if (not os.path.commonpath([dst_canon, unpack_canon])
               .startswith(unpack_canon)):
          ch.FATAL("can't COPY: destination not in image: %s" % dst_canon)
@@ -630,18 +630,20 @@ class I_from_(Instruction):
       images[image_i] = image
       if (self.alias is not None):
          images[self.alias] = image
-      ch.DEBUG("image path: %s" % image.unpack_path)
+      ch.VERBOSE("image path: %s" % image.unpack_path)
       # Other error checking.
       if (str(image.ref) == str(self.base_ref)):
          ch.FATAL("output image ref same as FROM: %s" % self.base_ref)
       # Initialize image.
       self.base_image = ch.Image(self.base_ref)
       if (not os.path.isdir(self.base_image.unpack_path)):
-         ch.DEBUG("image not found, pulling: %s" % self.base_image.unpack_path)
+         ch.VERBOSE("image not found, pulling: %s"
+                    % self.base_image.unpack_path)
          # a young hen, especially one less than one year old.
          pullet = pull.Image_Puller(self.base_image)
-         pullet.pull_to_unpacked(fixup=True)
+         pullet.pull_to_unpacked()
       image.copy_unpacked(self.base_image)
+      image.metadata_load()
       env.reset()
       # Find fakeroot configuration, if any.
       global fakeroot_config
@@ -761,15 +763,47 @@ class I_uns_yet(Instruction):
 ## Supporting classes ##
 
 class Environment:
-   "The state we are in: environment variables, working directory, etc."
-   __slots__ = ('arg', 'env', 'shell', 'workdir')
- 
+   """The state we are in: environment variables, working directory, etc. Most
+      of this is just passed through from the image metadata."""
+
+   __slots__ = ("arg",)
+
    def __init__(self):
       self.reset()
 
    @property
+   def env(self):
+      if (image_i == -1):
+         return dict()
+      else:
+         return images[image_i].metadata["env"]
+
+   @env.setter
+   def env(self, x):
+      images[image_i].metadata["env"] = x
+
+   @property
    def env_build(self):
       return { **self.arg, **self.env }
+
+   @property
+   def shell(self):
+      if (image_i == -1):
+         return ["/bin/false"]
+      else:
+         return images[image_i].metadata["shell"]
+
+   @shell.setter
+   def shell(self, x):
+      images[image_i].metadata["shell"] = x
+
+   @property
+   def workdir(self):
+      return ch.Path(images[image_i].metadata["cwd"])
+
+   @workdir.setter
+   def workdir(self, x):
+      images[image_i].metadata["cwd"] = str(x)
 
    def chdir(self, path):
       if (path.startswith("/")):
@@ -778,10 +812,8 @@ class Environment:
          self.workdir //= path
 
    def reset(self):
-      self.workdir = ch.Path("/")
-      self.shell   = ["/bin/sh", "-c"]
+      # This resets only things that aren't part of the image metadata.
       self.arg = { k: v for (k, v) in ARG_DEFAULTS.items() if v is not None }
-      self.env = { k: v for (k, v) in ENV_DEFAULTS.items() if v is not None }
 
 
 ## Supporting functions ###
