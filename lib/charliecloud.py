e9f9b534e5961359d39ef903a051287483cd4da6
Rusty Davis <rustyd@lanl.gov>
2021-03-16 14:34:03 -0400

    Update from old version

--- lib/charliecloud.py
+++ lib/charliecloud.py
@@ -151,7 +151,7 @@ WORD: /[^ \t\n=]/+
 
 _string_list: "[" _WS? STRING_QUOTED ( "," _WS? STRING_QUOTED )* _WS? "]"
 
-_NEWLINES: (_WS | "\n")+
+_NEWLINES: _WS? "\n" ("\n" | _WS)*
 _WS: /[ \t]|\\\n/+
 _LINE_CONTINUE: "\\\n"
 
