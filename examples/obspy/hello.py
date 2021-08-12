4eb9a67371a3fe70fa865b257e5551ed6482320d
Reid Priedhorsky <reidpr@lanl.gov>
2021-08-12 15:31:01 -0600

    tidy

--- examples/obspy/hello.py
+++ examples/obspy/hello.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 
-# "Reading Seismograms" example from §3 of the ObsPy tutorial.
+# "Reading Seismograms" example from §3 of the ObsPy tutorial, with some of
+# the prints commented out and taking the plot file from the command line.
+#
 # See: https://docs.obspy.org/tutorial/code_snippets/reading_seismograms.html
+
+import sys
+
 
 # §3.0
 
 from obspy import read
-import sys
 
-output_file = sys.argv[1]
-
-stream = read('RJOB_061005_072159.ehz.new')
-trace = stream[0]  # assign first and only trace to new variable
-print(trace)
+st = read('RJOB_061005_072159.ehz.new')
+#print(st)
+#print(len(st))
+tr = st[0]  # assign first and only trace to new variable
+print(tr)
 
 # §3.1
 
-print(trace.stats)
+print(tr.stats)
+#print(tr.stats.station)
+#print(tr.stats.datatype)
 
 # §3.2
 
-print(trace.data[0:3])
-print(len(trace))
+#print(tr.data)
+print(tr.data[0:3])
+print(len(tr))
 
 # §3.3
 
-trace.plot(outfile=output_file)
+tr.plot(outfile=sys.argv[1])
--- examples/obspy/test.bats
+++ examples/obspy/test.bats
@@ -5,22 +5,13 @@ CH_TEST_TAG=$ch_test_tag
 load "${CHTEST_DIR}/common.bash"
 
 setup () {
-    scope full
-    indir=${CHTEST_EXAMPLES_DIR}/obspy
-    inbind=${indir}:/mnt/0
-    outdir=$BATS_TMPDIR
-    outbind=${outdir}:/mnt/1
+    scope standard
     prerequisites_ok obspy
 }
 
 @test "${ch_tag}/hello" {
-    ch-run -b "$inbind" -b "$outbind" "$ch_img" -- sh -c "/hello.py \
-                                                   /mnt/1/obspy.png"
+    ch-run -b "$BATS_TMPDIR":/mnt "$ch_img" -- /hello.py /mnt/obspy.png
 
-    # Validate the plot is large enough for non-trivial content. (When I wrote
-    # this test, it was 74KiB.) Leave it for manual examination if desired.
-    [[ $(stat -c '%s' "$BATS_TMPDIR"/obspy.pdf) -gt 65536 ]]
-
-    # Compare the image to a previous run
-    cmp "${indir}/obspy.png" "${outdir}/obspy.png"
+    # Compare reference image to generated image.
+    cmp "$CHTEST_EXAMPLES_DIR"/obspy/obspy.png "$BATS_TMPDIR"/obspy.png
 }
