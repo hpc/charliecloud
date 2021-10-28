498d4eb266ce39d9b55455a47087ee943a440aa5
Jordan Ogas <jogas@lanl.gov>
2021-10-28 15:12:34 -0600

    fix typo

--- lib/build.py
+++ lib/build.py
@@ -266,7 +266,7 @@ class Instruction(abc.ABC):
 
    def metadata_add_history(self):
       hist = { "history": [ { "created": ch.now_utc_iso8601(),
-                              "created_by": "%s %s" % (self.str_name(), self.str_()),
+                              "created_by": "%s %s" % (self.str_name(), self.str_())} ]}
       images[image_i].metadata_append(hist)
 
    def options_assert_empty(self):
