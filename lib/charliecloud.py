443035c2bc67d3132db5ad94db534b870430b313
Jordan Ogas <jogas@lanl.gov>
2021-11-23 13:55:56 -0700

    PR #1227: ch-image: handle keyboard interrupt

--- lib/charliecloud.py
+++ lib/charliecloud.py
@@ -259,7 +259,10 @@ class Credentials:
          except KeyError:
             # Finally, prompt the user.
             # FIXME: This hangs in Bats despite sys.stdin.isatty() == True.
-            username = input("\nUsername: ")
+            try:
+               username = input("\nUsername: ")
+            except KeyboardInterrupt:
+               FATAL("authentication cancelled")
             password = getpass.getpass("Password: ")
          if (not password_many):
             # Remember the credentials.
