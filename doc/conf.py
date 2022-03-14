e954b8cb71c3fe73b3f934e5086e340d13fcfe3d
Reid Priedhorsky <1682574+reidpr@users.noreply.github.com>
2022-03-14 10:36:03 -0600

    PR #1300: update copyright

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Charliecloud'
-copyright = u'2014–2021, Triad National Security, LLC'
+copyright = u'2014–2022, Triad National Security, LLC and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
