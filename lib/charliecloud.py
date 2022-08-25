ef503e561d920de68fce4c03b4e9fd641f5ac342
Lucas Caudill <lcaudill@lanl.gov>
2022-08-25 20:55:58 +0000

    Remove Lark version ceiling

--- lib/charliecloud.py
+++ lib/charliecloud.py
@@ -44,7 +44,7 @@ depfails = []
 # [1]: https://github.com/lark-parser/lark/issues/505
 import lark
 LARK_MIN = (0,  7, 1)
-LARK_MAX = (1, 1, 2)
+LARK_MAX = (99, 0, 0)
 lark_version = tuple(int(i) for i in lark.__version__.split("."))
 if (not LARK_MIN <= lark_version <= LARK_MAX):
    depfails.append(("bad", 'found Python module "lark" version %d.%d.%d but need between %d.%d.%d and %d.%d.%d inclusive' % (lark_version + LARK_MIN + LARK_MAX)))
