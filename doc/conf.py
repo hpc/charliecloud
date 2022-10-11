d8d9941e97db4423b37150d08a26f0e2d227f375
Reid Priedhorsky <reidpr@lanl.gov>
2022-10-11 16:34:27 -0600

    fix tests?

--- doc/conf.py
+++ doc/conf.py
@@ -25,8 +25,16 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx_reredirects', 'sphinx.ext.mathjax', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo']
 todo_include_todos = True
+# Workaround for EPEL, which prohibits pip and doesn’t have
+# sphinx_reredirects. The result will be no redirect, but a lot about the
+# EPEL-built docs is crappy.
+try:
+   import sphinx_reredirects
+   extensions.append('sphinx_reredirects')
+except ImportError:
+   pass
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
