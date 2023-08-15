fba513c6ef9f07ede5ec9d4818f3c507598fbbeb
Lucas Caudill <lcaudill@lanl.gov>
2023-08-15 16:03:16 +0000

    ugghh

--- bin/ch_fuse.h
+++ bin/ch_fuse.h
@@ -1,9 +1,7 @@
 /* Copyright © Triad National Security, LLC, and others. */
 
 #define _GNU_SOURCE
-#include <squashfuse/ll.h>
 
 /** Function prototypes **/
 
 void sq_fork(struct container *c);
-sqfs_ll *sqfs_ll_open_(const char *path, size_t offset);
