4d605226e423894d90931c7f9a8ef199b547c59f
Reid Priedhorsky <reidpr@lanl.gov>
2023-09-06 13:02:40 -0600

    docs for commit 5d8f4c25

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.33',
+    VERSION: '0.34~pre+5d8f4c25',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
