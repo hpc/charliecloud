b5b1690bc6430b24ed4fdb4538844050ce468c11
Reid Priedhorsky <1682574+reidpr@users.noreply.github.com>
2023-11-27 14:49:35 -0700

    PR #1777: implement missed Path.iterdir()

--- lib/filesystem.py
+++ lib/filesystem.py
@@ -877,6 +877,16 @@ class Path(os.PathLike):
       except OSError as x:
          ch.FATAL("can’t read %s: %s" % (self, x.strerror))
 
+   def iterdir(self):
+      """e.g.:
+
+           >>> import os
+           >>> dir = Path("/proc/self/task")
+           >>> set(dir.iterdir()) == { dir // str(os.getpid()) }
+           True"""
+      for entry in ch.ossafe("can’t scan: %s" % self, os.scandir, self):
+         yield self.__class__(entry.path)
+
    def json_from_file(self, msg):
       ch.DEBUG("loading JSON: %s: %s" % (msg, self))
       text = self.file_read_all()
