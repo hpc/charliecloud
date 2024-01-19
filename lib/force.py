4b87d0a0a947a5f2bf0b6710d5574cfb8a268292
Lucas Caudill <lcaudill@lanl.gov>
2024-01-19 16:15:11 -0700

    PR #1812: make --force=fakeroot appropriately quiet for RHEL

--- lib/force.py
+++ lib/force.py
@@ -183,7 +183,7 @@ FAKEROOT_DEFAULT_CONFIGS = {
    { "name": "RHEL 7 and derivatives",
      "match": ("/etc/redhat-release", r"release 7\."),
      "init": [ ("command -v fakeroot > /dev/null",
-                "set -ex; "
+                "set -e; "
                 "if ! grep -Eq '\[epel\]' /etc/yum.conf /etc/yum.repos.d/*; then "
                 "yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm; "
                 "yum install -y fakeroot; "
@@ -198,7 +198,7 @@ FAKEROOT_DEFAULT_CONFIGS = {
    { "name": "RHEL 8+ and derivatives",
      "match":  ("/etc/redhat-release", r"release (?![0-7]\.)"),
      "init": [ ("command -v fakeroot > /dev/null",
-                "set -ex; "
+                "set -e; "
                 "if ! grep -Eq '\[epel\]' /etc/yum.conf /etc/yum.repos.d/*; then "
                 # Macro %rhel from *-release* RPM, e.g. redhat-release-server
                 # or centos-linux-release; thus reliable.
