8192b5bd47620c0071fd01e5d81a7774c6dd659f
London Bielicke <london.bielicke1313@gmail.com>
2024-07-08 15:20:38 -0600

    version cleanup and implemented all values from proto file

--- grpc_server/test/test_grpc_server.py
+++ grpc_server/test/test_grpc_server.py
@@ -1,32 +1,41 @@
-import grpc 
+# created from cri.proto file 
 import cri_pb2_grpc
 import cri_pb2
+
+import grpc 
 from concurrent import futures
 import subprocess 
-from subprocess import PIPE, Popen
-# this is an implementation of CRI Servicer from the .proto file
+
+# organise version information to return as protobuf
+VERSION_INFO = { "KubeVersion": "v1",           # version of the kubelet runtime api
+                 "RuntimeName": "Charliecloud", # name of the container runtime (const)
+                 "RuntimeApiVersion": "v0" }    # api version of the container runtime (I do not really know what this is)
+
+# this is an implementation of CRI's RuntimeService:
+# Runtime service defines the public APIs for remote container runtimes 
 class RuntimeServiceServicer(cri_pb2_grpc.RuntimeServiceServicer):
-   # implements gret version grpc function from .proto file
+   # Version returns the runtime name, runtime version and runtime API version.
    def Version(self, request, context):
-      # debug 
-      print("version")
-      # hardcode version
-      response = cri_pb2.VersionResponse(version="1.0.0")
-      # this is the Charliecloud command we want
+      print("version") # DEBUG
+      response = cri_pb2.VersionResponse(version=VERSION_INFO["KubeVersion"], runtime_name = VERSION_INFO["RuntimeName"], runtime_api_version=VERSION_INFO["RuntimeApiVersion"])
+
+      # this is the Charliecloud command we run to get the ch-run version
       cmd = "/usr/local/src/charliecloud/bin/ch-run"
       ca = [cmd, "--version"]
-      test = subprocess.check_output(ca, stderr=subprocess.STDOUT)
-      response.version = test 
+      
+      # for some reason version response is in STDERR
+      output = subprocess.check_output(ca, stderr=subprocess.STDOUT) 
+      response.runtime_version = output.rstrip() # note: rstrip removes new line
+
       return response
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # reference proto file
     cri_pb2_grpc.add_RuntimeServiceServicer_to_server(RuntimeServiceServicer(), server)
-    server.add_insecure_port('[::]:50052')
+    server.add_insecure_port(f'unix:///tmp/test.sock')
     server.start()
     print("Server listening on port 50052...") # DEBUG
-    server.wait_for_termination() # Listen
+    server.wait_for_termination() 
 
 if __name__ == '__main__':
     serve()
