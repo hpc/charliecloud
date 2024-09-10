7a9f9c161e443e045774689f0d60b07558d7f33d
Reid Priedhorsky <reidpr@lanl.gov>
2024-09-10 10:10:08 -0600

    docs for commit a617293

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.37',
+    VERSION: '0.38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
