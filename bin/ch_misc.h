f48db3f72e5511674ceb65719671771ae5b36eac
Reid Priedhorsky <reidpr@lanl.gov>
2024-09-16 13:23:49 -0600

    restore copyright notice in ch_misc.h too [skip ci]

--- bin/ch_misc.h
+++ bin/ch_misc.h
@@ -1,4 +1,6 @@
-/* This interface contains miscellaneous utility features. It is separate so
+/* Copyright © Triad National Security, LLC, and others.
+
+   This interface contains miscellaneous utility features. It is separate so
    that peripheral Charliecloud C programs don't have to link in the extra
    libraries that ch_core requires. */
 
